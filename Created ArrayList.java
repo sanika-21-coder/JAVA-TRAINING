cfaf87c22ae936f045d8a91247fa5ff70d188d20
Sanika Shriram Chavhan <sanikachavhan1631@gmail.com>
2026-06-06 13:43:43 +0530

    Create Created ArrayList.java

---
+++ Created ArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+        }
+    }
+    
+}
